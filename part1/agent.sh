93b8fe503936d3c967be2306f87cd2d51ffc5b93
Kernicterus <kernicterus@kernicterus.com>
2025-01-31 17:47:32 +0100

    part 1 ok

--- part1/agent.sh
+++ part1/agent.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 GREEN='\033[0;32m'
+RED='\033[0;31m'
 NC='\033[0m'
+DIR='vagrant'
 
-export TOKEN="token123456"
+apt-get update &&  apt upgrade -y
+apt install curl apt-transport-https -y
+export TOKEN=$(cat /${DIR}/k3s_token.txt)
+echo -e "${RED}token ${TOKEN}${NC}"
 curl -sfL https://get.k3s.io | K3S_URL="https://192.168.56.110:6443" K3S_TOKEN="${TOKEN}" INSTALL_K3S_EXEC="agent --node-ip 192.168.56.111" sh -
 echo -e "${GREEN}K3s agent installed and connected to the server${NC}"
