ad8765e49e74f406914b612b78120a91d498b7bd
Kernicterus <votre.email@example.com>
2025-01-31 22:16:27 +0100

    app1 part2

--- part2/server.sh
+++ part2/server.sh
@@ -8,21 +8,8 @@ DIR='vagrant'
 
 apt-get update &&  apt upgrade -y
 apt install curl apt-transport-https -y
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --node-ip 192.168.56.110 --write-kubeconfig-mode 0644" sh -s -
-# curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --node-ip 192.168.56.110 --write-kubeconfig-mode 0644 --token $TOKEN" sh -s -
-sleep 5
-sudo k3s kubectl get node
+curl -sfL https://get.k3s.io | sh -
+
 sleep 5
 
-TOKEN_FILE="/var/lib/rancher/k3s/server/node-token"
-while [ ! -f "$TOKEN_FILE" ]; do
-    echo "node-token creation..."
-    sleep 2
-done
-echo -e "${LPURP}K3s server installed${NC}"
-echo $(cat ${TOKEN_FILE}) > "/${DIR}/k3s_token.txt"
-if [ -e /${DIR}/k3s_token.txt ]; then
-    echo -e "${LPURP}SUCCESS K3s token copied to /${DIR}/k3s_token.txt${NC}"
-else
-    echo -e "${RED}FAIL to copy K3s token to /${DIR}/k3s_token.txt${NC}"
-fi
+kubectl apply -f https://k8s.io/examples/application/deployment.yaml
