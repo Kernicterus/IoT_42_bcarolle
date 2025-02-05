b8f130e59cd8cd35199dfb4e2996c474343ff14e
Kernicterus <kernicterus@kernicterus.com>
2025-02-05 17:46:51 +0100

    part3 ports

--- part3/scripts/startk3d.sh
+++ part3/scripts/startk3d.sh
@@ -3,14 +3,15 @@ GREEN='\033[0;32m'
 RED='\033[0;31m'
 LPURP='\033[1;35m'
 NC='\033[0m'
+CLUSTER_NAME=clusterNledent
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 FILE="${DIR}/../confs/cluster.yaml"
 
 
 echo -e "${LPURP}k3d cluster creation ... ${NC}"
-k3d cluster create mycluster --image rancher/k3s:v1.26.0-k3s1
+k3d cluster create $CLUSTER_NAME --image rancher/k3s:v1.26.0-k3s1 --config $DIR/../confs/cluster.yaml
 echo -e "${GREEN}k3d cluster creation completed ! ${NC}"
 export KUBECONFIG=~/.kube/config
 
-bash $DIR/config.sh
+bash $DIR/config.sh $CLUSTER_NAME
