b10912aaf13da5050b6be83a4ed4da76425c284b
kernicterus <me@example.com>
2025-02-12 15:49:28 +0100

    upd p2

--- p2/scripts/server.sh
+++ p2/scripts/server.sh
@@ -11,21 +11,21 @@ apt install curl apt-transport-https -y
 curl -sfL https://get.k3s.io | sh -
 
 sleep 5
-sudo chmod 755 ../confs
-sudo chown vagrant:vagrant ../confs
+sudo chmod 755 "/confs"
+sudo chown vagrant:vagrant "/confs"
 
-kubectl apply -f ../confs/pvolume.yaml
+kubectl apply -f "/confs/pvolume.yaml"
 sleep 2
-kubectl apply -f ../confs/pvc-vol-httpd.yaml
+kubectl apply -f "/confs/pvc-vol-httpd.yaml"
 
-kubectl apply -f ../confs/depl-portainer.yaml
-kubectl apply -f ../confs/depl-graf.yaml
-kubectl apply -f ../confs/depl-httpd.yaml
+kubectl apply -f "/confs/depl-portainer.yaml"
+kubectl apply -f "/confs/depl-graf.yaml"
+kubectl apply -f "/confs/depl-httpd.yaml"
 
-kubectl apply -f ../confs/app1-service.yaml
-kubectl apply -f ../confs/app2-service.yaml
-kubectl apply -f ../confs/app3-service.yaml
+kubectl apply -f "/confs/app1-service.yaml"
+kubectl apply -f "/confs/app2-service.yaml"
+kubectl apply -f "/confs/app3-service.yaml"
 
-kubectl apply -f ../confs/ingress.yaml
+kubectl apply -f "/confs/ingress.yaml"
 
 # kubectl apply -f /confs/nodePort-httpd.yaml
